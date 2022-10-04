6ae62dfe647919d61d0bc4c7ec009dba6ca19868
VsevolodZharkov <94003560+VsevolodZharkov@users.noreply.github.com>
2022-10-04 15:24:04 +0300

    save

--- models/contacts.js
+++ models/contacts.js
@@ -1,55 +1,28 @@
-const fs = require("fs/promises");
-const path = require("path");
-const contactsPath = path.resolve("./models/contacts.json");
-const { nanoid } = require("nanoid")
+const { Schema, model } = require("mongoose");
 
-const getAll = async () => {
-	const data = await fs.readFile(contactsPath, 'utf-8');
-  return JSON.parse(data);
-}
+const contactSchema = new Schema({
+	name: {
+		type: String,
+		required: [true, 'Set name for contact'],
+	},
+	email: {
+		type: String,
+	},
+	phone: {
+		type: String,
+	},
+	favorite: {
+		type: Boolean,
+		default: false,
+	},
+});
 
-const getContactById = async (contactId) => {
-	const data = await getAll();
-	const result = data.find(item => item.id === String(contactId));
-	return result || null;
-}
+const handleSaveError = (error, _, next) => {
 
-const removeContact = async (contactId) => {
-	const data = await getAll();
-	const indexContactDelete = data.findIndex(item => item.id === String(contactId));
-	const [result] = data.splice(indexContactDelete, 1);
-	await fs.writeFile(contactsPath, JSON.stringify(data, null, 2))
-	return result;
-}
+	next()
+};
 
-const addContact = async ({name, email, phone}) => {
-	const data = await getAll();
-	const newContact = {
-		"id": nanoid(),
-    name,
-    email,
-    phone: phone,
-	}
-	data.push(newContact)
-	await fs.writeFile(contactsPath, JSON.stringify(data, null, 2))
-	return newContact;
-}
+contactSchema.post("save", handleSaveError);
+const Contact = model("contacts", contactSchema);
 
-const updateContact = async (id, {name, phone, email}) => {
-	const contacts = await getAll();
-	const index = contacts.findIndex(item => item.id === id);
-	if(index === -1) {
-		return null;
-	}
-	contacts[index] = {id, name, phone, email};
-	await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
-	return contacts[index]
-}
-
-module.exports = {
-  getAll,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+module.exports = Contact;
