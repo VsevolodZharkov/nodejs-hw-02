656668b148b5e9d4d9fa01fa909ed8b8ae974308
VsevolodZharkov <94003560+VsevolodZharkov@users.noreply.github.com>
2022-10-04 14:05:40 +0300

    save

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-const { DB_HOST } = require("./config")
+const { DB_HOST } = process.env;
 const mongoose = require("mongoose")
 
 mongoose.connect(DB_HOST)
--- config.js
+++
@@ -1,5 +0,0 @@
-const DB_HOST = "mongodb+srv://Vsevolod:srJcxP7GF7QP2lVS@cluster0.jonxm0t.mongodb.net/?retryWrites=true&w=majority";
-
-module.exports = {
-	DB_HOST
-};
