05d65c2dbc11de0b4ba40ee7bfe3109749d132c9
VsevolodZharkov <94003560+VsevolodZharkov@users.noreply.github.com>
2022-10-04 17:38:31 +0300

    save

--- middleware/handelSaveError.js
+++ middleware/handelSaveError.js
@@ -1,6 +1,8 @@
 const isConflict = ({name, code}) => (name === "MangoServerError" && code === 11000);
 
 const handleSaveError = (error, _, next) => {
+	console.log(error.name);
+	console.log(error.code);
 	error.status = isConflict(error) ? 409 : 400;
 	next();
 };
