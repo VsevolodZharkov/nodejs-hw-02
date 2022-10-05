ca937674beb4080ee42b40265adb23a7fc55d41b
VsevolodZharkov <94003560+VsevolodZharkov@users.noreply.github.com>
2022-10-05 12:35:12 +0300

    save

--- models/contact.js
+++ models/contact.js
@@ -21,8 +21,6 @@ const contactSchema = new Schema({
 const isConflict = ({name, code}) => (name === "MangoServerError" && code === 11000);
 
 const handleSaveError = (error, _, next) => {
-	console.log(error.name);
-	console.log(error.code);
 	error.status = isConflict(error) ? 409 : 400;
 	next();
 };
