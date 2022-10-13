10dc28dab80fd83424999c9c9480aedea0142173
vsevolodjarkov <94003560+vsevolodjarkov@users.noreply.github.com>
2022-10-13 19:52:17 +0300

    save

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,8 +1,7 @@
 const { User } = require("../../models/user");
 const fs = require("fs/promises");
 const path = require("path");
-const { RequestError } = require("../../helpers");
-
+const Jimp = require('jimp');
 const avatarDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async(req, res) => {
@@ -13,6 +12,13 @@ const updateAvatar = async(req, res) => {
 		const fileName = `${_id}.${extention}`;
 		const resultUpload = path.join(avatarDir, fileName);
 		await fs.rename(temUpload, resultUpload);
+
+		Jimp.read(resultUpload, async (err, avatar) => {
+			if (err) throw err;
+			 await avatar
+					 .resize(256, 256) // resize
+					.write(resultUpload); // save
+	});
 		const avatarURL = path.join("avatar", fileName);
 		await User.findByIdAndUpdate(_id, {avatarURL});
 		
@@ -21,7 +27,7 @@ const updateAvatar = async(req, res) => {
 		})
 	} catch (error) {
 		await fs.unlink(req.file.path);
-		throw new RequestError(401, "Not authorized");
+		throw error;
 	}
 }
 
